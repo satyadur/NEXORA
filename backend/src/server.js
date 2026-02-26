8191a852ec0c2ca2224a45df601fe64b9b4a7636
satyadur <satyakudupudi8500@gmail.com>
2026-02-26 16:18:36 +0530

    added chnaged

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,7 +20,8 @@ app.use((err, req, res, next) => {
 
   next();
 });
+const PORT = process.env.PORT || 5000;
 
-app.listen(process.env.PORT, () =>
-  console.log(`Server running on port ${process.env.PORT}`)
-);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
